8bd9344cfc467620185a5fc08a4dabc198c19f6d
h2ck4u <h2ckforu@gmail.com>
2025-12-12 17:09:17 +0900

    feat: add giscus

--- src/features/post-comments/ui/post-comments.tsx
+++ src/features/post-comments/ui/post-comments.tsx
@@ -5,10 +5,10 @@ export default function GiscusComments() {
   const { theme } = useTheme();
   return (
     <Giscus
-      repo="h2ck4u/notion-blog-nextjs-giscus"
-      repoId="R_kgDOOHw4QQ"
+      repo="h2ck4u/blog"
+      repoId="R_kgDOQnWp7A"
       category="Announcements"
-      categoryId="DIC_kwDOOHw4Qc4Cn9v2"
+      categoryId="DIC_kwDOQnWp7M4CzsZh"
       mapping="pathname"
       strict="0"
       reactionsEnabled="1"
